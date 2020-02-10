f79295e4e685b01373ee524a9f595fe3fa68724f
Chris Johnson <chrisjohnson00@hotmail.com>
2020-02-09 16:18:26 -0800

    Simplifying the chart packaging for future charts to be added

--- package_charts.sh
+++ package_charts.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -x
 
-body='{
- "request": {
- "message": "Auto Triggered Rebuild",
- "branch":"master"
-  }
-}'
 
-curl --fail -i -X POST \
- -H "Content-Type: application/json" \
- -H "Accept: application/json" \
- -H "Travis-API-Version: 3" \
- -H "Authorization: token $travis_token" \
- -d "$body" \
- https://api.travis-ci.com/repo/chrisjohnson00%2Fhelm-charts/requests
+## declare an array variable
+declare -a charts=("awx-proxy-chart" "cadvisor-chart")
+
+## now loop through the above array
+for i in "${charts[@]}"
+do
+  echo "Start of $i"
+  git clone https://$GITHUB_TOKEN@github.com/chrisjohnson00/$i.git
+  cd $i
+  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.0.2 lint .
+  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.0.2 package .
+  mv *.tgz ../.
+  cd ..
+  sudo rm -r $i
+  echo "End of $i"
+done
