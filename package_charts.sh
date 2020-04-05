d4e5a7a9ade5bbded525523370c8431cf681ab31
Chris Johnson <csjohnson3@wsgc.com>
2020-04-05 09:56:06 -0700

    Added handbrake chart

--- package_charts.sh
+++ package_charts.sh
@@ -3,7 +3,7 @@ set -x
 
 
 ## declare an array variable
-declare -a charts=("cadvisor-chart" "flask-chart")
+declare -a charts=("cadvisor-chart" "flask-chart" "handbrake-watcher-chart")
 
 ## now loop through the above array
 for i in "${charts[@]}"
@@ -11,8 +11,8 @@ do
   echo "Start of $i"
   git clone https://$GITHUB_TOKEN@github.com/chrisjohnson00/$i.git
   cd $i
-  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.0.2 lint .
-  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.0.2 package .
+  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.1.2 lint .
+  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.1.2 package .
   mv *.tgz ../.
   cd ..
   sudo rm -r $i
