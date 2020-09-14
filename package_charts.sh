c815af993e392928a4ce8e92467d618687faa355
Chris Johnson <chrisjohnson00@hotmail.com>
2020-09-13 19:11:15 -0700

    adding qbittorrent chart and upping helm version

--- package_charts.sh
+++ package_charts.sh
@@ -3,7 +3,7 @@ set -x
 
 
 ## declare an array variable
-declare -a charts=("cadvisor-chart" "flask-chart" "handbrake-watcher-chart" "x-arr-chart")
+declare -a charts=("cadvisor-chart" "flask-chart" "handbrake-watcher-chart" "x-arr-chart" "qbittorrentvpn-chart")
 
 ## now loop through the above array
 for i in "${charts[@]}"
@@ -11,8 +11,8 @@ do
   echo "Start of $i"
   git clone https://$GITHUB_TOKEN@github.com/chrisjohnson00/$i.git
   cd $i
-  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.3.0 lint .
-  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.3.0 package .
+  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.3.1 lint .
+  docker run --rm -v ${PWD}:/chart -w /chart alpine/helm:3.3.1 package .
   mv *.tgz ../.
   cd ..
   sudo rm -r $i
